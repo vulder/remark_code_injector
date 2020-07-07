b4ad4cb10d92163008a94b68c8a4464117fa27bb
Florian Sattler <vuld3r@gmail.com>
2020-07-07 21:23:25 +0200

    Basic test setup

---
+++ cmd/remark-inject-code/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/vulder/remark_code_injector/internal"
+
+func main() {
+	fmt.Println("Hello World.")
+	fmt.Println(internal.Hello())
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vulder/remark_code_injector
+
+go 1.15
